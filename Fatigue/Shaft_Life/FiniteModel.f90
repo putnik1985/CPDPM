c23247a6874809ce6ae090562375dfa21446b433
putnik1985 <put_nik4@mail.ru>
2024-05-24 12:25:56 -0400

    change KE for bendig calculations only

--- Fatigue/Shaft_Life/FiniteModel.f90
+++ Fatigue/Shaft_Life/FiniteModel.f90
@@ -1,7 +1,5 @@
        SUBROUTINE FORM_LOCAL_KE_BEAM(EJ,L,K)
 
-       ! BEGIN DESCRIPTION OF THE VARAIBLES
-
        implicit none
        real*8 EJ, L 
        real*8 K(4,4)
@@ -9,22 +7,22 @@
 
        call Matrix_Zero(4,K)
 
-       KE(2,2) = 12 * E * JZ / L**3
-       KE(2,6) = 6 * E * JZ / L**2 
-       KE(2,8) = -12 * E * JZ / L**3 
-       KE(2,12) = 6 * E * JZ / L**2 
+       KE(1,1) = 12 * EJ / L**3
+       KE(1,2) = 6 * EJ / L**2 
+       KE(1,3) = -12 * EJ / L**3 
+       KE(1,4) = 6 * EJ / L**2 
 
-       KE(6,6) = 4 * E * JZ / L
-       KE(6,8) = -6 * E * JZ / L**2
-       KE(6,12) = 2 * E * JZ / L
+       KE(2,2) = 4 * EJ / L
+       KE(2,3) = -6 * EJ / L**2
+       KE(2,4) = 2 * EJ / L
 
-       KE(8,8) = 12 * E * JZ /L**3
-       KE(8,12) = -6 * E * JZ / L**2
+       KE(3,3) = 12 * EJ /L**3
+       KE(3,4) = -6 * EJ / L**2
        
        DO I=1,4
-       DO J=I,4
-       K(J,I)=K(I,J)
-       ENDDO ! DO J
+          DO J=I,4
+             K(J,I)=K(I,J)
+          ENDDO ! DO J
        ENDDO !DO I
 							 
        END SUBROUTINE FORM_LOCAL_KE_BEAM
@@ -52,14 +50,14 @@
        subroutine Add_Global(i,j,KE,K,N)
         implicit none
         real*8 KE(4,4)
-        real*8 K(2*N,2*N)
+        real*8 K(N,N)
         integer r, c, N
         do r = 1,2 
          do c = 1,2 
-         K(6*i-6+r,6*i-6+c) = K(6*i-6+r,6*i-6+c) + KE(r,c)
-         K(6*i-6+r,6*j-6+c) = K(6*i-6+r,6*j-6+c) + KE(r,c+6)
-         K(6*j-6+r,6*i-6+c) = K(6*j-6+r,6*i-6+c) + KE(r+6,c)
-         K(6*j-6+r,6*j-6+c) = K(6*j-6+r,6*j-6+c) + KE(r+6,c+6)
+         K(2*i-2+r,2*i-2+c) = K(2*i-2+r,2*i-2+c) + KE(r,c)
+         K(2*i-2+r,2*j-2+c) = K(2*i-2+r,2*j-2+c) + KE(r,c+2)
+         K(2*j-2+r,2*i-2+c) = K(2*j-2+r,2*i-2+c) + KE(r+2,c)
+         K(2*j-2+r,2*j-2+c) = K(2*j-2+r,2*j-2+c) + KE(r+2,c+2)
          enddo
         enddo 
       end subroutine Add_Global
