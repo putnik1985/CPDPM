1f810285894008181e4ebad76452f274d8b4faef
Nikita Putin <nikita.putin@mda.space>
2024-10-15 17:40:32 -0400

    gauss'

---
+++ Numerical_Library/gauss.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <errno.h>
+
+
+/* gauss: solve linear system of n equations Ax=b */
+double* gauss(int n, double A[n][n], double b[n])
+{
+        for(int i = 0; i < n - 1; ++i)
+		for(int j = i + 1; j < n; ++j){
+			double factor = A[j][i] / A[i][i];
+			for(int k = i; k < n; ++k)
+				A[j][k] -= factor * A[i][k];
+
+			b[j] -= factor * b[i];
+		}
+
+	double* x = calloc(n, sizeof(double));
+	if ( x == NULL) return NULL;
+
+	for(int k = n - 1; k >= 0; --k){
+		double s = 0.;
+		for(int j = k + 1; j < n; ++j)
+			s += A[k][j] * x[j];
+		x[k] = (b[k] - s) / A[k][k];
+	}
+	return x;
+}
---
+++ Numerical_Library/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+double* gauss(int n, double a[n][n], double b[n]); 
+
+int main()
+{
+	int n = 3;
+	double a[3][3] = {{2.,-1.,-1.},
+		          {3.,4.,-2.},
+			  {3.,-2.,4.}};
+
+	double b[3] = {4.,11.,11.};
+
+	double* x;
+	x = gauss(n,a,b);
+
+	if ( x == NULL) 
+		return -1;
+
+	for(int i = 0; i<n; ++i)
+		printf("%g,", x[i]);
+
+	printf("\n");
+	return 0;
+}
