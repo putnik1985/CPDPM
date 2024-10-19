aaaad81398fe025418e938cb1a2de6e818986a24
putnik1985 <put_nik4@mail.ru>
2024-10-18 22:02:12 -0400

    implemented c functions nto c++ code

--- Numerical_Library/gauss.c
+++ Numerical_Library/gauss.c
@@ -1,13 +1,13 @@
 #include "numeric_c.h"
 
 /* gauss: solve linear system of n equations Ax=b */
-double* gauss(int n, double A[n][n], double b[n])
+double* gauss(int n, double* A, double* b)
 {
         for(int i = 0; i < n - 1; ++i)
 		for(int j = i + 1; j < n; ++j){
-			double factor = A[j][i] / A[i][i];
+			double factor = A[j*n + i] / A[i*n + i];
 			for(int k = i; k < n; ++k)
-				A[j][k] -= factor * A[i][k];
+				A[j*n + k] -= factor * A[i*n + k];
 
 			b[j] -= factor * b[i];
 		}
@@ -18,8 +18,8 @@ double* gauss(int n, double A[n][n], double b[n])
 	for(int k = n - 1; k >= 0; --k){
 		double s = 0.;
 		for(int j = k + 1; j < n; ++j)
-			s += A[k][j] * x[j];
-		x[k] = (b[k] - s) / A[k][k];
+			s += A[k*n + j] * x[j];
+		x[k] = (b[k] - s) / A[k*n + k];
 	}
 	return x;
 }
