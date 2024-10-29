0b089d1522c6e2bbbc8c98d3a6b16312362fe7fd
Nikita Putin <nikita.putin@mda.space>
2024-10-29 16:12:56 -0400

    go practice

--- Go-practice/go-book-ex-4-1.go
+++ Go-practice/go-book-ex-4-1.go
@@ -3,9 +3,20 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
+	s := 256;
+
+	if (len(os.Args) > 1){
+        	flag := os.Args[1];
+	        fmt.Printf("flag: %s\n", flag);
+                s, _ = strconv.Atoi(flag);
+	        fmt.Printf("flag: %d\n", s);
+        }
+
 	var count int;
 	c1 := sha256.Sum256([]byte("x"))
 	c2 := sha256.Sum256([]byte("X"))
