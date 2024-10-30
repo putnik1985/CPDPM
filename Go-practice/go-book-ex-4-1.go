257425755b5e444b84c124ceb81d3f72b6768708
Nikita Putin <nikita.putin@mda.space>
2024-10-30 07:45:58 -0400

    go practice

--- Go-practice/go-book-ex-4-1.go
+++ Go-practice/go-book-ex-4-1.go
@@ -3,27 +3,49 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
-	"os"
-	"strconv"
 )
 
 func main() {
-	s := 256;
+            var count byte
+	    count = 0
 
-	if (len(os.Args) > 1){
-        	flag := os.Args[1];
-	        fmt.Printf("flag: %s\n", flag);
-                s, _ = strconv.Atoi(flag);
-	        fmt.Printf("flag: %d\n", s);
-        }
-
-	var count int;
-	c1 := sha256.Sum256([]byte("x"))
-	c2 := sha256.Sum256([]byte("X"))
-	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
-	for i, _ := range c1 {
-		fmt.Printf("%d = %b, %b\n", i, c1[i], c2[i]);
-		// split on bits and comparer bit by bit, if different ++count
-		count++;
-	}
+	    c1 := sha256.Sum256([]byte("x"))
+	    c2 := sha256.Sum256([]byte("X"))
+	    fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
+            for i, _ := range c1 {
+		code1 := convert(c1[i])
+		code2 := convert(c2[i])
+		fmt.Printf(" first: %b\n", code1);
+		fmt.Printf("second: %b\n", code2);
+		diff := count_different_bits(code1, code2)
+		fmt.Printf("bits: %d\n", diff)
+		count += diff
+            }
+	    fmt.Printf("number of different bits: %d\n", count)
 }
+
+func convert(x uint8) [8]byte {
+     var code [8]byte
+     var i byte;
+     i = 7;
+     for x>0 {
+	     code[i] = x % 2;
+	     x >>= 1;
+	     i--;
+     }
+     return code;
+}
+
+func count_different_bits(code1 [8]byte, code2 [8]byte) byte {
+     var count byte
+     count = 0
+
+     for i, _ := range code1 {
+	     if (code1[i] != code2[i]){
+	         count++
+	     }
+	 }
+
+     return count
+}
+
