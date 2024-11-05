5acd71af4aacbb8dda96d97d274a2cd59b14d4cd
Nikita Putin <nikita.putin@mda.space>
2024-11-05 14:15:06 -0500

    go comma

--- Go-practice/go-book-comma.go
+++ Go-practice/go-book-comma.go
@@ -18,9 +18,20 @@ func comma(s string) string {
          var parts []string = strings.Split(s,".")
 
 	    main := parts[0]
-	 decimal := parts[1]
-	 var pos int = 0
+	    //fmt.Printf("%T\n",main)
+	    decimal := ""
+            sign := ""
 
+	    if (len(parts) > 1){
+	        decimal = parts[1]
+	     }
+         ///fmt.Printf("%t\n",main[0]=='-')
+         if (main[0] == '-'){
+		 main = main[1:]
+		 sign = "-"
+	 }
+
+	 var pos int = 0
          l := len(main)
          for i, _ := range main {
 		 if (pos < 2){
@@ -33,7 +44,23 @@ func comma(s string) string {
 		 }
 	 }
 
-	 buf.WriteByte('.')
-	 buf.WriteString(decimal)
-	 return buf.String()
+         temp := buf.String()
+	 //fmt.Printf("%s\n", temp)
+	 if (temp[len(temp)-1] == ','){
+		 temp = temp[:len(temp)-1]
+	 }
+
+	 //fmt.Printf("%s\n", temp)
+	 rs := reverse(temp)
+	 return sign + rs + "." + decimal
+}
+
+func reverse(s string) string {
+     var buf bytes.Buffer
+
+     l := len(s)
+     for i, _ := range s {
+         buf.WriteByte(s[l - 1 - i])
+     }
+     return buf.String()
 }
