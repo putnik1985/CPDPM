426dc7f10ac23370a745a5848b0cf00659e50fdc
Nikita Putin <nikita.putin@mda.space>
2025-02-28 09:11:02 -0500

    random

--- Random_Vibrations/random.go
+++ Random_Vibrations/random.go
@@ -4,6 +4,9 @@ import(
 	"fmt"
 	"os"
 	"strings"
+	"bufio"
+	"strconv"
+	"math"
 )
 
 func main() {
@@ -12,6 +15,7 @@ func main() {
 		fmt.Println("usage: ./random file=input.pch")
 		return
 	}
+
 	data := make(map[string]string)
 
 	/****************************
@@ -21,7 +25,6 @@ func main() {
 	****************************/
 
 	for _, value := range os.Args {
-		fmt.Println(value)
 		parts := strings.Split(value, "=")
 		if (len(parts) > 1) {
 			 data[parts[0]] = parts[1]
@@ -34,7 +37,45 @@ func main() {
 		fmt.Printf("%s=%s\n",name, value)
 	}
 	*******************************************************/
+
 	filename := data["file"]
-	fmt.Printf("file: %s\n", filename)
+	////fmt.Printf("Input file: %s\n", filename)
+
+	f, err := os.Open(filename)//f becomes *os.File
+	if err != nil {
+		  fmt.Fprintf(os.Stderr, "random: %v\n", err)
+	}
+        var eigenvalues []float64
+	input := bufio.NewScanner(f)
+	/////fmt.Printf("%T\n", input)
+	for input.Scan() {
+            line, fields := readline(input) 
+	    if strings.Contains(line, "$EIGENVALUE") {
+	       ////fields := strings.Fields(line)
+	       /*****************************************************************
+	       for index, value := range fields {
+	           fmt.Printf("%d = %s, ", index, value)
+	       }
+	       fmt.Printf("\n");
+	       ******************************************************************/
+	       value, err := strconv.ParseFloat(fields[2], 64)
+	       if err != nil {
+		       fmt.Fprintf(os.Stderr, "random: %v\n", err)
+		       return
+	       }
+               eigenvalues = append(eigenvalues, value)
+            }
+	    fmt.Printf("\n")
+	}
+        f.Close()
+	fmt.Printf("Frequencies:\n");
+	for index, value := range eigenvalues {
+	    fmt.Printf("#%d = %g\n", index, math.Sqrt(value) / (2. * math.Pi))
+	}
 	return
+}
+
+func readline(s *bufio.Scanner) (string, []string) {
+	line := s.Text()
+	return line, strings.Fields(line)
 }
