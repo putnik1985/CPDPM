76399779059845d54cdf96e3fd9a01a304a37588
Nikita Putin <nikita.putin@mda.space>
2025-03-13 11:41:20 -0400

    plates

--- Plates/static-axisymetric-plates.go
+++ Plates/static-axisymetric-plates.go
@@ -6,15 +6,15 @@ import(
 	"strings"
 	"bufio"
 	"strconv"
+	"math"
 )
 
 func main(){
 	//fmt.Println(os.Args[1:])
 	data := make(map[string]string)
-	boundary := make(map[string](func(r float64) (float64, float64, float64)))
+	boundary := make(map[string](func(r float64) (float64, float64, float64, float64)))
 
 	boundary["moment"] = moment
-	 boundary["shear"] = shear
 	 boundary["angle"] = angle
 	  boundary["disp"] = disp
 
@@ -51,29 +51,54 @@ func main(){
 	    h, _ := strconv.ParseFloat(data["h"], 64)
 	   nu, _ := strconv.ParseFloat(data["nu"], 64)
 	  rho, _ := strconv.ParseFloat(data["rho"], 64)
+	    q, _ := strconv.ParseFloat(data["q"], 64)
 
 	  fmt.Printf("\nInput Data:\n")
-	  fmt.Printf("E = %g, h = %f, rho = %g, nu = %f\n", E, h, rho, nu)
+	  fmt.Printf("E = %g, h = %f, rho = %g, nu = %f, q = %f\n", E, h, rho, nu, q)
 
 }
 
-
-func moment(r float64) (float64, float64, float64) {
+func moment(r float64) (float64, float64, float64, float64) {
 	fmt.Println("calculate moment")
-	return 0., 0., 0.
+	//q := float64(1.0)
+	D := float64(1.0)
+        nu := float64(1.0)
+        c1, c2, c3, b := dangle_over_dr(r)
+	d1, d2, d3, c :=   angle_over_r(r)
+	return D*(c1 + nu*d1), D*(c2 + nu*d2), D*(c3 + nu*d3), D*(b + nu*c) 
 }
 
-func shear(r float64) (float64, float64, float64) {
+func shear(r float64) (float64, float64, float64, float64) {
 	fmt.Println("calculate share")
-	return 0., 0., 0.
+	q := float64(1.0)
+	//D := float64(1.0)
+	return 0., 0., 0., q*r/2.
 }
 
-func angle(r float64) (float64, float64, float64) {
+func angle(r float64) (float64, float64, float64, float64) {
 	fmt.Println("calculate angle")
-	return 0., 0., 0.
+	q := float64(1.0)
+	D := float64(1.0)
+	return -r/2., -1/r, 0., -q*r*r*r/(16.*D)
 }
 
-func disp(r float64) (float64, float64, float64) {
+func disp(r float64) (float64, float64, float64, float64) {
 	fmt.Println("calculate displacement")
-	return 0., 0., 0.
+	q := float64(1.0)
+	D := float64(1.0)
+	return r * r / 4., math.Log(r), 1., q*r*r*r*r/(64.*D)
+}
+
+func dangle_over_dr(r float64) (float64, float64, float64, float64) {
+	fmt.Println("calculate angle")
+	q := float64(1.0)
+	D := float64(1.0)
+	return -1./2., 1./(r*r), 0., -3.*q*r*r/(16.*D)
+}
+
+func angle_over_r(r float64) (float64, float64, float64, float64) {
+	fmt.Println("calculate angle")
+	q := float64(1.0)
+	D := float64(1.0)
+	return -1/2., -1./(r*r), 0., -q*r*r/(16.*D)
 }
