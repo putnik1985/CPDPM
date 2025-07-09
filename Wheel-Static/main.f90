3832fbc4b0eec34a34540be18d4c63a6a0e0a6cc
Nikita Putin <nikita.putin@mda.space>
2025-07-09 08:28:31 -0400

    wheel

--- Wheel-Static/main.f90
+++ Wheel-Static/main.f90
@@ -8,8 +8,8 @@ program wheel
         parameter (Pi = 3.14159)
         common alpha, beta
         real w1, w2, d1w1, d1w2, d2w1, d2w2, d3w1, d3w2
-        real C0, C1, C2
-        real Q, M
+        real C0, C1, C2, c11, c22
+        real Q, M, Pc, disp, ang_disp
         real, allocatable:: T(:,:)
         real, allocatable:: b(:)
 
@@ -35,16 +35,23 @@ program wheel
          beta = sqrt((a + 1.) / 2.)
         !!!write(*,*) "Interstage calculations:" 
         !!!!write(*,'(A12,F12.2,A12,F12.4,A12,F12.4)') "a=",a, "alpha=", alpha, "beta=", beta
-        write(*,'(A12,A12,A12,A12,A12)') "Angle,deg", "Disp, cm", "Rotat, rad", "Q, kgs", "M, kgs.cm"
+        write(*,'(A12,A12,A12,A12,A12,A12)') "Angle,deg", "Disp, cm", "Rotat, rad", "Q, kgs", "M, kgs.cm", "Pc, kgs"
         C2 = ( p * R**3 / (E*J) ) / (d3w2(Pi) + d1w2(Pi) - d1w2(Pi) / d1w1(Pi) * (d3w1(Pi) + d1w1(Pi)))
         C1 = -C2 * d1w2(Pi) / d1w1(Pi)
-        C0 = -C1 * w1(Pi) - C2 * w2(Pi)
+
+        c11=alpha/(alpha**2+beta**2)*sinh(alpha*Pi)*cos(beta*Pi)+beta/(alpha**2+beta**2)*cosh(alpha*Pi)*sin(beta*Pi)
+        c22=alpha/(alpha**2+beta**2)*cosh(alpha*Pi)*sin(beta*Pi)-beta/(alpha**2+beta**2)*sinh(alpha*Pi)*cos(beta*Pi)
+
+        C0 = (-C1 * c11 - C2 * c22) / Pi
         sectors = 60
         do i = 0, sectors
            phi = -Pi + i * 2 * Pi / sectors
+           disp = C0 + C1 * w1(phi) + C2 * w2(phi)
+           ang_disp = C1 * d1w1(phi) + C2 * d1w2(phi)
            Q = E*J/R**3 * (C1 * (d3w1(phi) + d1w1(phi)) + C2 * (d3w2(phi) + d1w2(phi)))
            M = - E*J/R**2 * (C1 * (d2w1(phi) + w1(phi)) + C2 * (d2w2(phi) + w2(phi)))
-           write(*, 100) (phi + Pi) * 180 / Pi, C0 + C1 * w1(phi) + C2 * w2(phi), C1 * d1w1(phi) + C2 * d1w2(phi), Q, M
+           Pc = E * F / R * disp 
+           write(*, 100) (phi + Pi) * 180 / Pi, disp, ang_disp, Q, M, Pc
         enddo   
-100     Format(F12.2,F12.3,F12.6,F12.2,F12.2)
+100     Format(F12.2,F12.3,F12.6,F12.2,F12.2,F12.2)
 end program
