b23609fd0315f1c49ab5a7e586f559508eace537
putnik1985 <put_nik4@mail.ru>
2025-08-10 09:34:34 -0400

    Ewins

--- Ewins-Modal-Analysis/main.cpp
+++ Ewins-Modal-Analysis/main.cpp
@@ -3,21 +3,46 @@
 #include <complex>
 #include <map>
 #include <fstream>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
+
+class receptance {
+  public:
+  complex<double> operator()(double w);
+  receptance(double m1, double k1, double c1);
+
+  private:
+  double m;
+  double k;
+  double c;
+  double w0;
+
+};
+
+complex<double> receptance::operator()(double w){
+                return 1. / ( k - w*w * m + 1.i * w * c);
+}
+
+receptance::receptance(double m1, double k1, double c1):
+m(m1), k(k1), c(c1) 
+{
+       w0 = sqrt(k/m);
+}
 
 int main(int argc, char** argv){
 
     map<string, string> data;
     for(auto i = 1; i < argc; ++i){
         string s = argv[i];
-        cout << s << '\n'; 
+        ///cout << s << '\n'; 
         auto pos = s.find("=");
 
         if (pos > 0) {
             auto key = s.substr(0, pos);
             auto value = s.substr(pos+1, s.size());
-            cout << key <<  " " << value << '\n';
+            //cout << key <<  " " << value << '\n';
             data[key] = value;
         }
     }
@@ -31,9 +56,42 @@ int main(int argc, char** argv){
              cerr << "can not open file: " << filename << '\n';
              return 1;
     }
+
     string line;
+    map<string, double> model;
+
     while (getline(in, line)){
-           cout << line << '\n';
+           ///cout << line << '\n';
+           auto pos = line.find(",");
+           auto key = line.substr(0, pos);
+           ////cout << key << endl; 
+           auto value = line.substr(pos+1);
+           ////cout << stod(value) << endl;
+           model[key] = stod(value);
     }
+    in.close();
+    for( auto& record : model){
+         ///cout << record.first << record.second << endl; 
+    }
+    auto ksi = model["ksi"];
+    auto k = model["k"];
+    auto g = model["g"];
+    auto f = model["f"];
+    auto fmax = model["fmax"];
+    ////cout << ksi << k << g << f << fmax << endl;
+    int N = int(fmax);
+    auto m = k * g / (4 * M_PI * M_PI * f * f );
+    //cout << "mass(kgs/g) = " << m/g << endl;
+    auto c = ksi * 2. * sqrt(m * k / g );
+    receptance alpha(m/g, k, c);
+   
+    double df = fmax / N;
+        for(auto i = 0.; i <= N; ++i){
+            double w = 2 * M_PI * df * i;
+            auto value = alpha(w);
+            cout << setw(12) << fixed << setprecision(4) << df * i
+                 << setw(12) << fixed << setprecision(4) << abs(value)
+                 << endl;
+        } 
     return 0;
 }
