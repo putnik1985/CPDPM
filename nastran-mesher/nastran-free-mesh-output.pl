9f4383473ba9de287ef721a776477992501415ed
Nikita Putin <nikita.putin@mda.space>
2025-08-22 13:27:54 -0400

    mesher

--- nastran-mesher/nastran-free-mesh-output.pl
+++ nastran-mesher/nastran-free-mesh-output.pl
@@ -6,8 +6,7 @@ use Cwd qw(getcwd);
     print "\n";
 ###usage: perl nastran-free-mesh-modifier.pl file_where_mesh file_where_commands scale_factor_for_length
 	my $mesh_file = $ARGV[0];
-    my $command_file = $ARGV[1];
-	###my $scale = $ARGV[2];
+
 	
 	my $rbe2_fields = 9;
 	my $rbe3_fields = 9;
@@ -84,14 +83,20 @@ use Cwd qw(getcwd);
 	$next_spc++;
 
     my @output;
+	my @fields;
 	
+	     print "List of commands:\n";
+		 print "grid,20,25 - output grids from 20 to 25 inclusive\n";
+		 print "rbe2,inputfile - create rbe2 for the grids from input file\n";
+		 print "write,outputfile - write the results of all commands into outputfile\n";
+		 
          while(<STDIN>){
          my $line = $_;
 		 chomp($line);
 		 my @words = split/,/,$line;
 		 ####print $words[0], $words[1];
 		 print "\n";
-		 if ($line =~ /grid/){ ##usage grid,20,25
+		 if ($line =~ /^grid/){ ##usage grid,20,25
 		       for (my $i=$words[1]; $i <= $words[2]; $i++) {
 				    print $file_grids{$i};
                     my @coord = split/,/,$file_grids{$i};
@@ -99,15 +104,185 @@ use Cwd qw(getcwd);
 			   }
 		 }
 
-         if ($line =~ /write/){ ## write,output.dat
+         if ($line =~ /^write/){ ## write,output.dat
 			 my $filename = $words[1];
 			 ####print $filename;
 			 open(outputh, '>', $filename);
 			 for (@output) {
-				 printf outputh "%s\n", $_;
+				 printf outputh "%s", $_;
 			 }
 			 close(outputh);
+		 }
+
+         if ($line =~ /^rbe2/){ ## rbe2,fileofnodes
+		     my $list = $words[1];
+             &nastran_rbe2("rbe2,$list,0.001,");
 		 }
 		 
 		 print "\n";	   
          }
+		 
+sub nastran_rbe2 {
+
+	       my @words = split /,/,@_[0];
+		   my $list = $words[1];
+		   my $scale = $words[2];
+
+           my ($x0, $y0, $z0) = &cg_from_list($list);
+		   $x0 *= $scale;
+		   $y0 *= $scale;
+		   $z0 *= $scale;
+		   
+		   &grid_create_rbe2_from_list("_grid_create_rbe2_from_list,$list,$x0,$y0,$z0,");
+
+}		 
+
+sub nastran_rbe3 {
+        my $grid = @_[0];
+        my $element = @_[1];
+        my @grids;
+        for(my $i=2; $i<@_; $i++){
+            push(@grids,@_[$i]);
+            ####print @_[$i] . "\n";
+        }
+
+        my $current_field = 8;
+	my $current = 0;
+	my $last = @grids;
+        #####print @grids;
+	printf "RBE3,%d,,%d,123456,1.0,123,",$element,$grid;
+	for(my $k = 0; $k < $last; $k++) {
+
+			if ($current_field <= $rbe3_fields) {
+		        printf "%d,",$grids[$k];
+				++$current_field;
+			} else {
+		        printf "+\n";
+		        printf "+,";
+		        printf "%d,",$grids[$k];				
+                $current_field = 3; 				
+            }
+    }	
+}
+
+sub cg_from_list{
+	
+my $list = @_[0]; ## read filename
+my @grids;
+           open(gridsh, "<", $list) or die $!;
+           my $count = 0;
+           my $x0;
+           my $y0;
+           my $z0;
+		   		   
+ 	       while (<gridsh>) {
+			 if ($_ =~ /Global coordinates/){
+				 $_ =~ s/^\s+|\s+$//g; ## remove leading and trailing spaces
+				 my @words = split/\s+/,$_;
+                 my $x = $words[3];
+				 my $y = $words[4];
+				 my $z = $words[5];
+				 $x0 += $x;
+				 $y0 += $y;
+				 $z0 += $z;
+				 $count++;
+			 }
+		    }
+			close(gridsh);
+			$x0 /= $count;
+			$y0 /= $count;
+			$z0 /= $count;
+			return($x0, $y0, $z0);
+}
+
+sub fields{
+    @fields = split/,/,@_[0];
+}
+
+sub cg {
+
+    my @grids;
+    my $xc;
+    my $yc;
+    my $zc;
+
+    for(my $i=0; $i < @_; $i++){
+        push(@grids, @_[$i]);
+    }
+
+    for (@grids) {
+         my $grid = $_;
+         &fields($file_grids{$grid});
+         $xc += $fields[3];
+         $yc += $fields[4];
+         $zc += $fields[5]; 
+    } 
+    $xc /= @grids;
+    $yc /= @grids;
+    $zc /= @grids;
+    ($xc, $yc, $zc)
+}
+
+sub grid_create_rbe2_from_list {
+
+	       my @words = split /,/,@_[0];
+		   my $list = $words[1];
+		   my $x0 = $words[2];
+		   my $y0 = $words[3];
+		   my $z0 = $words[4];
+  
+           my @grids;
+           open(gridsh, "<", $list) or die $!;
+	       while (<gridsh>) {
+			 my @words = split / /,$_;
+			 if ($words[0] =~ /Label|label/){
+                 push(@grids,$words[1]);
+			 }
+		    }
+	       close(gridsh);
+	open(h1, ">>", $mesh_file) or die $!;
+
+	my $output_str = sprintf "\n\$_grid_create_rbe2_from_list:\n";
+	push(@output,$output_str);
+
+    my $current_field = 5;
+	my $current = 0;
+	my $last = @grids;
+    
+
+	$output_str = sprintf "GRID,%d,0,%f,%f,%f,0\n",$next_grid,$x0,$y0,$z0;
+	push(@output, $output_str);
+	print $output_str;
+	
+	$output_str = sprintf "RBE2,%d,%d,123456,",$next_element,$next_grid;
+	push(@output, $output_str);
+	print $output_str;
+	$next_grid++;
+	$next_element++;
+	for(my $k = 0; $k < $last; $k++) {
+
+			if ($current_field <= $rbe2_fields) {
+		        $output_str = sprintf "%d,",$grids[$k];
+				push(@output, $output_str);
+				print $output_str;
+				
+				++$current_field;
+			} else {
+		        $output_str = sprintf "+\n";
+				push(@output, $output_str);
+				print $output_str;
+				
+		        $output_str = sprintf "+,";
+				push(@output, $output_str);
+				print $output_str;
+				
+		        $output_str = sprintf "%d,",$grids[$k];				
+				push(@output, $output_str);
+				print $output_str;
+				
+                $current_field = 3; 				
+            }
+
+    }
+
+}
