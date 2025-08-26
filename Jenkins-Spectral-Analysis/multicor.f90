920f34b88c222ab9a81bc7034ea56f75fa1dd438
Nikita Putin <nikita.putin@mda.space>
2025-08-26 09:30:14 -0400

    Jenkins

--- Jenkins-Spectral-Analysis/multicor.f90
+++ Jenkins-Spectral-Analysis/multicor.f90
@@ -8,7 +8,7 @@ program multicor
         real, allocatable, dimension(:,:) :: x
         real, allocatable, dimension(:,:,:) :: cov, dcov, cor, dcor
         character*8, allocatable, dimension(:) :: ident 
-        character*256 filename
+        character*256 filename, titles
         integer i, j, k, L
 
         print*
@@ -18,11 +18,13 @@ program multicor
         read*, filename
 
         open(unit = 12, file = filename)
+        read(12,*) titles
         read(12,*) NS, N
         MAXM = N - 1 
 
         allocate(ident(NS), x(N,NS), xm(NS))
-        allocate(cov(0:MAXM,NS,NS))
+        allocate(cov(0:MAXM+1,NS,NS), dcov(0:MAXM, NS, NS))
+        allocate(cor(0:MAXM,NS,NS), dcor(0:MAXM, NS, NS))
 
         read(12,*) ident(:)
         do i=1,N
@@ -30,14 +32,11 @@ program multicor
         enddo
         close(12)
 
-         do i=1, N
-           write(*,*) x(i,:)
-        enddo
-        
-        print*
+        call output_vector(8, N, NS, x, ident, "series.dat")  
+
         do j=1,NS
            xm(j) = average(N, x(:,j))
-           write(*,*) "Serie: ", ident(j), "Average: ", xm(j)
+           print*, xm(j)
         enddo
 
         do j=1,NS
@@ -46,11 +45,8 @@ program multicor
            enddo
         enddo
 
-        print*
-        print*,"Normalized series:"
-        do i=1, N
-           write(*,*) x(i,:)
-        enddo
+
+        call output_vector(9, N, NS, x, ident, "norm-series.dat")  
 
         do j=1,NS
            do L=1,NS
@@ -64,12 +60,78 @@ program multicor
             enddo
         enddo
               
-        print*
-        print*,"Covariance:"
-        do k=0,MAXM+1
-           write(*,*) k, cov(k,1,:)
+        do j=1,NS
+           do L=1,NS
+              do k=0,MAXM
+                 dcov(k,j,L) = -cov(k-1, j,L) + 2*cov(k,j,L)-cov(k+1,J,L)
+              enddo
+            enddo
         enddo
 
-        deallocate(x, ident, xm, cov)
+        do j=1,NS
+           do L=1,NS
+              do k=0,MAXM
+                 cor(k,j,L) = cov(k,j,L) / sqrt(cov(0,j,j) * cov(0,L,L)) 
+                 dcor(k,j,L) = dcov(k,j,L) / sqrt(dcov(0,j,j) * dcov(0,L,L)) 
+              enddo
+            enddo
+        enddo
+
+        call output(12, MAXM+1, NS, cov, "covariance.dat")
+        call output(14, MAXM, NS, dcov, "diff-covariance.dat")
+        call output(16, MAXM, NS, cor, "correlation.dat")
+        call output(24, MAXM, NS, dcor, "diff-correlation.dat")
+
+        deallocate(x, ident, xm, cov, dcov, cor, dcor)
 end program
 
+subroutine output(unit1, MAXM, NS, cov, filename)
+        integer MAXM, NS, j, L, unit1
+        character*256 filename
+        real, dimension(0:MAXM, NS, NS) :: cov
+
+        open(unit = unit1, file = filename)
+        write(unit1,"(A12)", advance = "no") "Lug"
+        do j=1,NS
+           do L=1,NS
+              write(unit1,"(A12I1I1A1)", advance = "no") "COV(",j,L,")"
+           enddo
+        enddo
+        write(unit1,*) 
+              do k=0,MAXM
+                 write(unit1,'(I12)', advance="no") k
+                 do j=1,NS
+                    do L=1,NS
+                       write(unit1,'(F15.2)', advance = "no") cov(k,j,L)
+                    enddo
+                 enddo   
+                 write(unit1,*) 
+              enddo
+        close(unit1)
+
+end subroutine
+
+subroutine output_vector(unit, N, NS, x, ident, filename)
+         implicit none
+         integer i, N, unit, j, NS
+         real, dimension(N, NS) :: x
+         character*8, dimension(NS) :: ident
+         character*256 filename
+   
+          
+         open(unit = unit, file = filename)
+         write(unit, '(A12)', advance="no") "Lug"
+         do j=1, NS
+           write(unit, '(A12)', advance = "no") ident(j)
+         enddo
+         write(unit,*)
+
+         do i=1, N
+           write(unit,'(I12)', advance = "no") i
+           do j=1,NS
+              write(unit,'(F12.2)', advance = "no") x(i,j)
+           enddo
+           write(unit,*)
+        enddo
+
+end subroutine
