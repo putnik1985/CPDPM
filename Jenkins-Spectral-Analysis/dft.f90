98be49e0caa95cc9fc42ac7773e391a84dcef29d
Nikita Putin <nikita.putin@mda.space>
2025-10-23 14:51:15 -0400

    dft

--- Jenkins-Spectral-Analysis/dft.f90
+++ Jenkins-Spectral-Analysis/dft.f90
@@ -18,16 +18,16 @@ program dft
       open(unit = 12, file = filename)
 
       read(12,*) str
-      read(12,*) delta, N
-      df = 1. / (2. * delta * N)
-      allocate(w(0:N), spec(0:N), cov(0:N-1))
+      read(12,*) delta, N, NF
+      df = 1. / (2. * delta * NF)
+      allocate(w(0:N), spec(0:NF), cov(0:N-1))
      
 	  time_energy = 0.
       read(12,*) str
       do i = 0, N-1
          read(12,*) value
          cov(i) = value
-		 time_energy = delta * value**2 + time_energy
+	 time_energy = delta * value**2 + time_energy
       enddo
 	     
       close(12)
@@ -36,17 +36,17 @@ program dft
       enddo
 
       open(unit = 12, file = "dft_output.dat")
-	  frequency_energy = 0.
+      frequency_energy = 0.
       write(12,'(A12A16)') "Freq(Hz)", "R(f)"
-      do I=0, N 
+      do I=0, NF 
          cossum = 0.
          sinsum = 0.
-         do k=1, N 
-            cossum = cossum + cov(k) * w(k) * cos(pi * k * i / N)
-            sinsum = sinsum + cov(k) * w(k) * sin(pi * k * i / N)
+         do k=0, N-1 
+            cossum = cossum + cov(k) * w(k) * cos(pi * k * i / NF)
+            sinsum = sinsum - cov(k) * w(k) * sin(pi * k * i / NF)
          enddo
-         ai = 2. * delta * (cov(0) + 2. * cossum) 
-         bi = 2. * delta * 2. * sinsum  
+         ai = sqrt(2.) * delta * cossum 
+         bi = sqrt(2.) * delta * sinsum  
          write(12,'(F12.4F16.8)') i*df, sqrt(ai**2 + bi**2) 
          frequency_energy = frequency_energy + (ai**2 + bi**2) * df
       enddo
--- Jenkins-Spectral-Analysis/signal.awk
+++ Jenkins-Spectral-Analysis/signal.awk
@@ -1,9 +1,9 @@
 BEGIN{
      pi = 3.14159
      delta = 0.0001
-	 print "Delta", "N"
-	 print delta, 1001
+	 print "Delta", "N", "NF"
+	 print delta, 1001, 1000
 	 print "cos"
      for(i=0;i<=1000;++i)
-         print sin(2*pi*12.*i*delta) * cos(2*pi*24.*i*delta)
+         print cos(2*pi*24.*i*delta)
 }
