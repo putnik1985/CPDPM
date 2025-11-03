38e9cade0b54db52c3bca251b700bb1c2f5a071c
Nikita Putin <nikita.putin@mda.space>
2025-11-03 09:42:21 -0500

    calculator

--- structural-calculator/structural-calculator.pl
+++ structural-calculator/structural-calculator.pl
@@ -10,14 +10,15 @@ use Cwd qw(getcwd);
 		 
 	     print "----------------------------------------------------\n";	
 	     print "List of commands:\n";
-         print "----------------------------------------------------\n";		 
+             print "----------------------------------------------------\n";		 
 		 print "circle,D       - output geometric properies of the circle\n";
 		 print "rectangle,a,b  - output geometric properies of the rectangle\n";
 		 print "mm2-to-in2,area - convert area in mm2 to area in in2\n";
 		 print "mm4-to-in4,inertia - convert inertia in mm4 to inertia in in4\n";
-         print "stress-area,D,n       - output stress area of the treads D, n - threads per in\n";		 
+                 print "stress-area,D,n    - output stress area of the treads D, n - threads per in\n";		 
+                 print "bolt-joint,P,Kb,Kj,Preload  - output bolt and joint loads when apply P external when prelaoded with Preload force\n";		 
 	     print "----------------------------------------------------\n";
-		 print "\n";
+      	     print "\n";
 		 
          while(<STDIN>){
          my $line = $_;
@@ -25,7 +26,17 @@ use Cwd qw(getcwd);
 		 my @words = split/,/,$line;
 		 ####print $words[0], $words[1];
 		 print "\n";
-
+         if ($line =~ /^bolt-joint/){
+		 my $P = $words[1];
+		 my $KB = $words[2];
+		 my $KJ = $words[3];
+		 my $Preload = $words[4];
+		 my $boltload = $Preload + $P * (1. / (1. + $KJ/$KB));
+		 my $jointload = $Preload - $P * (($KJ/$KB) / (1. + $KJ/$KB));
+		 my $sepload = $Preload * (1. + $KJ/$KB) / ($KJ/$KB);
+		 printf "%12s%12s%12s\n","Bolt Load", "Joint Load", "Separation Load";
+		 printf "%12.2f%12.2f%12.2f\n", $boltload, $jointload, $sepload
+	 }
          if ($line =~ /^stress-area/){
 			 my $D = $words[1];
 			 my $n = $words[2];
