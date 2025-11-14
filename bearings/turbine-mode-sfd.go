02d78199de5dd478c225a130ae2b0d7027918f7b
Nikita Putin <nikita.putin@mda.space>
2025-11-14 12:35:17 -0500

    bearings

--- bearings/turbine-mode-sfd.go
+++ bearings/turbine-mode-sfd.go
@@ -7,7 +7,7 @@ import(
 	"bufio"
 	"strconv"
 	"math"
-	//"math/cmplx"
+	"math/cmplx"
 )
 
 var line string
@@ -15,13 +15,14 @@ var fields []string
 var input *bufio.Scanner
 var filename string
 
-var Jd, Jp, m complex128 // mass and inertia of the turbine
-var speed complex128 // rotor rotational speed
-var L, b complex128 // distane to bearings, distance to CG of the turbine
-var ky, kz complex128 // stiffnesses in y and z direction
-var etay, etaz complex128 // loss factor for y and z direction
-var unb complex128 // unbalance on the turbine
-var dy, dz complex128 // damping in y and z direction, will be damper
+var Jd, Jp, m float64 // mass and inertia of the turbine
+var speed float64 // rotor rotational speed
+var L, b float64 // distane to bearings, distance to CG of the turbine
+var ky, kz float64 // stiffnesses in y and z direction
+var etay, etaz float64 // loss factor for y and z direction
+var unb float64 // unbalance on the turbine
+var dy, dz float64 // damping in y and z direction, will be damper
+var steps int = 1000
 
 
 const (
@@ -30,10 +31,15 @@ const (
 	max_iter = 1.E+6
 )
 
+type ncomplex struct {
+	r float64
+	im float64
+}
+
 func main() {
 
 	if (len(os.Args) < 2) {
-		fmt.Println("usage: ./turbine-mode-sfd command=command-file")
+		fmt.Println("usage: ./turbine-mode-sfd command=command-file steps=1000")
 		return
 	}
 
@@ -61,32 +67,50 @@ func main() {
 		 }
 	}
 	//fmt.Println(data)
-	Jd = complex(tonumber(data["Jp"]),0.)
-	Jp = complex(tonumber(data["Jd"]),0.) 
-	 m = complex(tonumber(data["m"]),0.)
+	Jd = tonumber(data["Jp"])
+	Jp = tonumber(data["Jd"]) 
+	 m = tonumber(data["m"])
 	 
- speed = complex(tonumber(data["speed"]),0.)
-     L = complex(tonumber(data["L"]),0.)
-	 b = complex(tonumber(data["b"]),0.)
+ speed = tonumber(data["speed"])
+     L = tonumber(data["L"])
+	 b = tonumber(data["b"])
 	 Jd = Jd + m * b * b
 
-    ky = complex(tonumber(data["ky"]),0.)
-    kz = complex(tonumber(data["kz"]),0.)
+    ky = tonumber(data["ky"])
+    kz = tonumber(data["kz"])
 
-  etay = complex(tonumber(data["etay"]),0.)
-  etaz = complex(tonumber(data["etaz"]),0.)	
+  etay = tonumber(data["etay"])
+  etaz = tonumber(data["etaz"])	
 
-   unb = complex(tonumber(data["unb"]),0.)
+   unb = tonumber(data["unb"])
 
-    dy = complex(tonumber(data["dy"]),0.)
-    dz = complex(tonumber(data["dz"]),0.)    
+    dy = tonumber(data["dy"])
+    dz = tonumber(data["dz"])    
 	
-    fmt.Println(m, Jp, Jd, speed, L, b, ky, kz, dy, dz, etay, etaz, unb)
+    ///fmt.Println(m, Jp, Jd, speed, L, b, ky, kz, dy, dz, etay, etaz, unb)
+    fmt.Printf("%12s%12s%12s\n","Time,sec","UY,mm","UZ,mm")
+
+    var x0 [2]complex128
+    var dx0 [2]complex128
+
+    var t float64 = 0.
+    var dt float64 = 0.0005 // 2kHz sample rate
+    var n int
+
+    n = steps
+    fmt.Printf("%12.6f%12.6f%12.6f\n", t, L*real(x0[1]), -L*real(x0[0]))
+    for i:=0; i<=n; i++ {
+	x, dx := df(t, dt, x0, dx0)
+	x0 = x
+	dx0 = dx
+        t += dt
+        fmt.Printf("%12.6f%12.6f%12.6f\n", t, L*real(x0[1]), -L*real(x0[0]))
+    }
     return
 }
 
 // working with 2DOF system
-func f(x [2]complex128, dx [2]complex128) [2]complex128 {
+func f(t float64, x [2]complex128, dx [2]complex128) [2]complex128 {
 	 var y [2]complex128
 	 w := speed * math.Pi / 30.
 	 
@@ -96,26 +120,25 @@ func f(x [2]complex128, dx [2]complex128) [2]complex128 {
 	 v1 := dx[0]
 	 v2 := dx[1]
 	 
-	 y[0] = -(0.*v1 + Jp*w * v2 + dz*L*L * v1 +     0. * v2 + kz*L*L * u1 +     0. * u2 + 1i*etaz*kz*L*L * u1 +             0. * u2) / Jd
-	 y[1] = -(-Jp*w*v1 + 0.* v2 +      0.* v1 + dy*L*L * v2 +     0. * u1 + ky*L*L * u2 +             0. * u1 + 1i*etay*ky*L*L * u2) / Jd
-	 
+	 y[0] = cmplx.Exp(1i*complex(w*t,0.))*complex(unb*w*w*L/Jd, 0.) * 1i + complex(-1./Jd,0.) * ( complex(Jp*w,0.)*v2 + complex(dz*L*L,0.)*v1 + complex(kz*L*L,0.)*u1 + 1i*complex(etaz*kz*L*L,0.)*u1)
+	 y[1] =      cmplx.Exp(1i*complex(w*t,0.))*complex(unb*w*w*L/Jd, 0.) + complex(-1./Jd,0.) * (complex(-Jp*w,0.)*v1 + complex(dy*L*L,0.)*v2 + complex(ky*L*L,0.)*u2 + 1i*complex(etay*ky*L*L,0.)*u2)
 	 return y
 }
 
 // runge kutt for 2nd order equation
 // create methods
-func df(dt float64, x0 [2]complex128, dx0 [2]complex128) ([2]complex128, [2]complex128) {
+func df(t float64, dt float64, x0 [2]complex128, dx0 [2]complex128) ([2]complex128, [2]complex128) {
 	var y [2]complex128
 	var dy [2]complex128
-	k1 := dt * f(x0, dx0)
-	k2 := dt * f(x0 + dt/2.*dx0 + dt/8.*k1, dx0 + k1/2.)
-	k3 := dt * f(x0 + dt/2.*dx0 + dt/8.*k2, dx0 + k2/2.)
-	k4 := dt * f(x0 + dt*dx0    + dt/2.*k3, dx0 + k3)
-	A := dx0 + 1./6. * (k1 + k2 + k3)
+
+	k1 := times(dt, f(t, x0, dx0))
+	k2 := times(dt, f(t + dt/2., sum(x0 ,times(dt/2.,dx0) ,times(dt/8.,k1)), sum(dx0, times(0.5, k1)) ))
+	k3 := times(dt, f(t + dt/2., sum(x0 ,times(dt/2.,dx0) ,times(dt/8.,k2)), sum(dx0, times(0.5, k2)) ))
+	k4 := times(dt, f(   t + dt, sum(x0 ,times(dt, dx0)   ,times(dt/2.,k3)), sum(dx0,    k3) ))
+	 A := sum( dx0, times(1./6., sum(k1, k2, k3)) )
 	
-	   y = x0 + dt * A
-	  dy = A + 1./6. * (k2 + k3 + k4)
-	  
+	 y = sum(x0,times(dt,A))
+	dy = sum( A,times(1./6.,sum(k2, k3, k4)))
 	return y, dy
 }
 
@@ -127,4 +150,18 @@ func tonumber(str string) float64 {
 	return num
 }
 
+func times(r float64, z [2]complex128) [2]complex128{
+	var out [2]complex128
+	out[0] = complex(r,0.) * z[0]
+	out[1] = complex(r,0.) * z[1]
+	return out
+}
 
+func sum(vals ...[2]complex128) [2]complex128 {
+	var total [2]complex128
+	for _, val := range vals {
+		total[0] += val[0]
+		total[1] += val[1]
+	}
+	return total
+}
