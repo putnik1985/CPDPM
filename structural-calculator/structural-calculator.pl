d6acf2079078750d426766c42b109812963def48
Nikita Putin <nikita.putin@mda.space>
2025-12-01 14:20:21 -0500

    calculator

--- structural-calculator/structural-calculator.pl
+++ structural-calculator/structural-calculator.pl
@@ -16,7 +16,8 @@ use Cwd qw(getcwd);
 		 print "mm2-to-in2,area - convert area in mm2 to area in in2\n";
 		 print "mm4-to-in4,inertia - convert inertia in mm4 to inertia in in4\n";
                  print "stress-area,D,n    - output stress area of the treads D, n - threads per in\n";		 
-                 print "bolt-joint,P,Kb,Kj,Preload  - output bolt and joint loads when apply P external when prelaoded with Preload force\n";		 
+                 print "bolt-joint,P,Kb,Kj,Preload  - output bolt and joint loads when apply P external when prelaoded with Preload force\n";	
+         print "outercircle,grids.txt       - calculate a center and a diameter of the outer circle for the 3 grids fron the file\n";				 
 	     print "----------------------------------------------------\n";
       	     print "\n";
 		 
@@ -37,6 +38,40 @@ use Cwd qw(getcwd);
 		 printf "%12s%12s%12s\n","Bolt Load", "Joint Load", "Separation Load";
 		 printf "%12.2f%12.2f%12.2f\n", $boltload, $jointload, $sepload
 	 }
+	     if ($line =~ /^outercircle/){
+			 my $grid_file = $words[1];
+			 my @coordinates;
+			 
+			 open(fh,'<',$grid_file);
+			      while (<fh>){
+					  chomp;
+					   my @numbers = split/\s/,$_;
+					   push(@coordinates,@numbers);
+				  }
+			 close(fh);
+
+			      my ($x1, $y1) = ($coordinates[0], $coordinates[1]);
+			      my ($x2, $y2) = ($coordinates[2], $coordinates[3]);
+			      my ($x3, $y3) = ($coordinates[4], $coordinates[5]);
+				  
+                  my $a = sqrt(($x2-$x3)**2 + ($y2 - $y3)**2);
+                  my $b = sqrt(($x3-$x1)**2 + ($y3 - $y1)**2);
+                  my $c = sqrt(($x1-$x2)**2 + ($y1 - $y2)**2);
+
+                  my $p = 0.5 * ($a + $b + $c);
+                  my $S = sqrt($p * ($p - $a) * ($p - $b) * ($p - $c));
+                  my $R = $a * $b * $c / (4. * $S);
+				  
+                  my $alpha = acos($c/(2.*$R));
+				  my  $beta = atan2(($y2-$y1),($x2 - $x1));
+				  
+				  my $x0 = $x1 + $R * cos($alpha + $beta);
+				  my $y0 = $y1 + $R * sin($alpha + $beta);
+				  
+				  
+				  printf "%12s%12s%12s\n", "R", "x0", "y0";
+                  printf "%12.4f%12.4f%12.4f\n", $R, $x0, $y0;  				  
+		 }
          if ($line =~ /^stress-area/){
 			 my $D = $words[1];
 			 my $n = $words[2];
