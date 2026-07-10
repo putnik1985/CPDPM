4444b598e1e19d396b643391022387a99578fae8
Nikita Putin <nikita.putin@mda.space>
2026-07-10 09:52:11 -0400

    tikhonov

--- Tikhonov-Reqularization/tikhonov.go
+++ Tikhonov-Reqularization/tikhonov.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 import "gonum.org/v1/gonum/mat"
 import "log"
+import  "bufio"
+import "strings"
+import _ "strconv"
+import "os"
+import "io"
 
 func main(){
         ///var input []float64={1., -1., 1., 1.,}
@@ -17,4 +22,57 @@ func main(){
 		log.Fatal("Eigendecomposition failed")
 	}
 	fmt.Printf("Eigenvalues of A:\n%v\n", eig.Values(nil))
+
+
+	data := make(map[string]string)
+	for _, value := range os.Args {
+		command := strings.Split(value,"=")
+		if (len(command) > 1) {
+			data[command[0]] = command[1]
+		}
+	}
+
+	file := data["file"]
+	fmt.Printf("Input file: %s\n", file)
+	handler, err := os.Open(file)
+	defer handler.Close()
+	if (err != nil){
+		message := fmt.Sprintf("can not read file: %s", file)
+		panic(message)
+	}
+	string_vector := read(handler, "vector")
+	string_n := read(handler, "dimension")
+	string_matrix := read(handler, "matrix")
+
+	fmt.Println(string_n, string_matrix, string_vector)
+
+}
+
+func read(handler io.Reader, key string) []string {
+        var output []string
+	scanner := bufio.NewScanner(handler)
+	var found bool = false
+	fmt.Println("looking for: ", key)
+	for scanner.Scan(){
+		line := scanner.Text()
+
+		if (strings.Contains(line, key)) {
+		    fmt.Printf("found %s\n",line)
+		    found = true
+		    continue
+	        } else if ( !found) {
+		    continue	
+		}
+
+
+		contains_numbers := strings.ContainsAny(line,"0123456789")
+		if (found && contains_numbers) {
+		    fmt.Printf("%s\n",line)	
+		    output = append(output, line)
+		} else {
+		    break
+		}
+	}
+
+	return output
 }
