b3ea2b9f53f5dd655e0cb05944e82d006f1c3b56
Nikita Putin <nikita.putin@mda.space>
2026-08-17 05:43:17 -0400

    beam shapes

--- structural-calculator/Krilov-functions-beam.f90
+++ structural-calculator/Krilov-functions-beam.f90
@@ -8,20 +8,20 @@ end function K0
 real function K1(alpha, x)
         real alpha
         real x
-        K1 = (1./(2.*alpha)) * (cosh(alpha*x) + cos(alpha*x))
+        K1 = (1./(2.*alpha)) * (sinh(alpha*x) + sin(alpha*x))
         return
 end function K1
 
 real function K2(alpha, x)
         real alpha
         real x
-        K2 = (1./(2.*alpha**2))* (cosh(alpha*x) + cos(alpha*x))
+        K2 = (1./(2.*alpha**2))* (cosh(alpha*x) - cos(alpha*x))
         return
 end function K2
 
 real function K3(alpha, x)
         real alpha
         real x
-        K3 = (1./(2.*alpha**3)) * (cosh(alpha*x) + cos(alpha*x))
+        K3 = (1./(2.*alpha**3)) * (sinh(alpha*x) - sin(alpha*x))
         return
 end function K3
